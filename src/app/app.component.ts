b779b9fca0e21f3015f1a173db9e22fa6f2d1ecd
Raul Balvin <raul.balvin@vasslatam.com>
2019-12-31 14:49:32 -0500

    Mostrar direccion ip publica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+'use strict'
+import { Component, OnInit } from '@angular/core';
+
+declare var require: any;
+const ipify = require('ipify2');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'get-ip';
+
+  ngOnInit(){
+    this.getIp()
+  }
+
+  async getIp(){
+    try {
+      let ipv4 = await ipify.ipv4();
+      if(ipv4) this.title=ipv4
+    } catch (error) {
+      console.log(error);
+    }
+    
+  }
 }
